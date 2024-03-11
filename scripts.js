dd38c95a97eef28169b32f4cf11c707757565b05
SirGeist <trinh1234533@yahoo.com>
2024-03-10 20:16:44 -0500

    Formatted the display so it is wider; replaced the bottom 2 buttons with 1 big equal sign button

--- scripts.js
+++ scripts.js
@@ -16,12 +16,22 @@ let calc = (num1, num2, operator) => {
 
 const calcContainer = document.querySelector(".btn-container");
 const fragment = document.createDocumentFragment();
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 19; i++) {
   const btn = document.createElement("button");
   btn.setAttribute(
     "style",
-    "display: flex; justify-content: space-between; height: 20px; align-items: center; padding: 20px;"
+    "display: flex; justify-content: center; align-items: center; padding: 40px;"
   );
+  btn.textContent = i;
+
+  // Last button is supposed to be the equal sign
+  if (i === 18) {
+    btn.textContent = "=";
+    btn.style.width = "calc(50% - 20px)";
+    btn.style.height = "calc(20% - 10px)";
+    btn.style.marginLeft = "5px";
+    btn.setAttribute("colspan", "2");
+  }
   fragment.appendChild(btn);
 }
 
